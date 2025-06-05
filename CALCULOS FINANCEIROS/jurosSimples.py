082ff46abd60c99174f72d2a56282aa79a966bc5
EmillynPanserini <emillyn2003@gmail.com>
2025-06-05 18:38:49 -0300

    metodos para montante e juros simples

--- CALCULOS FINANCEIROS/jurosSimples.py
+++ CALCULOS FINANCEIROS/jurosSimples.py
@@ -1,8 +1,12 @@
-class JurosSimples:
+class Juros_simples:
     def __init__(self):
-        self.valorJuros = 0
-        self.montanteFinal = 0
+        self.valor_juros = 0
+        self.montante_final = 0
 
-    def calcularJurosImples(self, capitalInicial,  taxaJuros, valorJuros, montanteFinal, tempo):
-        self.valorJuros = capitalInicial * taxaJuros *  tempo
-        montanteFinal = capitalInicial + valorJuros
+    def calcular_juros_simples(self, capital_inicial,  taxa_juros, valor_juros, montante_final, tempo):
+        self.valor_juros = capital_inicial * taxa_juros *  tempo
+
+        return self.valor_juros
+    def calcular_montante_final(self, capital_inicial, valor_juros):
+        self.montante_final = capital_inicial + valor_juros
+        return  self.montante_final
